efd6bc2569c378712746fdc4b1e8cc5bd31e52d2
Connor Taffe <cpt@wopr.byteflame.org>
2015-02-15 01:29:12 -0600

    update

--- dyndns_update.py
+++ dyndns_update.py
@@ -1,15 +1,15 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
 import requests, datetime
 
-urls = ['http://cpt.hopper.pw:LBbRhmu3gV@ipv4.www.hopper.pw/nic/update', 'http://%3Cyour%20fqdn%3E:%3Cyour%20secret%3E@ipv6.www.hopper.pw/nic/update']
+urls = ['http://cpt.hopper.pw:LBbRhmu3gV@ipv4.www.hopper.pw/nic/update'] #, 'http://%3Cyour%20fqdn%3E:%3Cyour%20secret%3E@ipv6.www.hopper.pw/nic/update']
 
 # basic auth to hopper.pw updates
-print "==========" # log separator
-print "date: %s" % datetime.datetime.now()
+print("==========") # log separator
+print ("date: ", datetime.datetime.now())
 for url in urls:
 	try:
 		r = requests.get(url, auth=('cpt.hopper.pw', 'LBbRhmu3gV'))
-		print r.text
+		print(r.text)
 	except requests.exceptions.RequestException as e:
-		print "error: %s" % e
+		print(e)
