ab57abbc6b815b4d5f892f0c4259642e354ab1d8
Connor Taffe <cpaynetaffe@gmail.com>
2015-05-01 15:25:04 -0500

    daemonized

--- dyndns_update.py
+++ dyndns_update.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 
-import requests, datetime
+import requests
+from time import sleep
 
-urls = ['http://cpt.hopper.pw:LBbRhmu3gV@ipv4.www.hopper.pw/nic/update'] #, 'http://%3Cyour%20fqdn%3E:%3Cyour%20secret%3E@ipv6.www.hopper.pw/nic/update']
+urls = ['http://cpt.hopper.pw:LBbRhmu3gV@ipv4.www.hopper.pw/nic/update']
 
 # basic auth to hopper.pw updates
-print("==========") # log separator
-print ("date: ", datetime.datetime.now())
-for url in urls:
-	try:
-		r = requests.get(url, auth=('cpt.hopper.pw', 'LBbRhmu3gV'))
-		print(r.text)
-	except requests.exceptions.RequestException as e:
-		print(e)
+while True:
+	for url in urls:
+		try:
+			r = requests.get(url, auth=('cpt.hopper.pw', 'LBbRhmu3gV'))
+		except requests.exceptions.RequestException as e:
+			print(e, file=sys.stderr) # print to stderr
+	sleep(300) # sleep for five minutes
