6d498eb7652a37263b55138909b1b7c83e72306d
Connor Taffe <cpaynetaffe@gmail.com>
2015-05-01 15:35:22 -0500

    updated logging

--- dyndns_update.py
+++ dyndns_update.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import requests
+import requests, datetime
 from time import sleep
 
 urls = ['http://cpt.hopper.pw:LBbRhmu3gV@ipv4.www.hopper.pw/nic/update']
@@ -10,6 +10,7 @@ while True:
 	for url in urls:
 		try:
 			r = requests.get(url, auth=('cpt.hopper.pw', 'LBbRhmu3gV'))
+			print("response @", datetime.datetime.now(), ":", r.text)
 		except requests.exceptions.RequestException as e:
 			print(e, file=sys.stderr) # print to stderr
 	sleep(300) # sleep for five minutes
